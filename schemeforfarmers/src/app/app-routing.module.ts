693f387448bb5a169b00fcfdc2d7352825d6f71a
Akhilesh1198 <akhilesh.cm@lntinfotech.com>
2021-01-12 02:36:06 +0530

    added bidderRegistrationForm

--- schemeforfarmers/src/app/app-routing.module.ts
+++ schemeforfarmers/src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'contact', component:ContactComponent}
+  {path: 'contact', component:ContactComponent},
+  {path:'bidderReg',component:BidderRegistrationComponent}
 ];
 
 @NgModule({
